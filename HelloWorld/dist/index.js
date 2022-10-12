c371f97d73a7e2db0deb2f2bc61a1a1f4514810f
AnasJMirza <ajmhimself@gmail.com>
2022-10-12 18:23:15 +0500

    arrays + tuple

--- HelloWorld/dist/index.js
+++ HelloWorld/dist/index.js
@@ -1,2 +1,7 @@
 "use strict";
-console.log("Hello World!!");
+let sales = 123324;
+let course = "JavaScript";
+let isPublished = true;
+let arr = [1, 2, 3];
+let tuple = [1, 'Anas'];
+console.log(arr, tuple);
