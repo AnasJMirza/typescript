459b7fab53b6b0bf6d83635bce8ffcbda9f7d80f
AnasJMirza <ajmhimself@gmail.com>
2022-11-17 21:21:40 +0500

    functinos

---
+++ functions/index.ts
@@ -0,0 +1,11 @@
+function greetings(msg:string): number {
+    return 1;
+    
+}
+
+const login = (name: string, email: string, isPaid: boolean = true) => {
+    return 1;
+};
+login("Anas", "anas@gmail.com")
+
+greetings("Hello, Anas!");
