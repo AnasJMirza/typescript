74085a4ac7accdc0e32d1add76de92d14de3e5a7
AnasJMirza <ajmhimself@gmail.com>
2022-11-17 23:37:53 +0500

    unions + arrays

---
+++ functions/union.ts
@@ -0,0 +1,10 @@
+let arr: number[] = [1,2,3,4]; // simple array decleration.
+let arr2: (string | number)[] = [1,2,3,4,4,'3']; // mix of both values in the array.
+let arr3: string[] | number[] = []; // Either the array can be of numbers or the strings.
+
+let seatAlocation: "window" | "middle" = "window" // yoy can define the value that is going to be in the variable. Other then   that no value is going to be in the variable.
+
+
+
+
+
